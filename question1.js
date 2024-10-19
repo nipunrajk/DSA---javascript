bfa215798d935c9404f434cdf39273d3d69ceba5
Nipunraj <nipunraj@mozilor.com>
2024-10-19 13:06:45 +0530

    descripion changed.

--- question1.js
+++ question1.js
@@ -1,6 +1,31 @@
-/* Function that accept 2 arrays 
-   and return true if the second array contain the squared value of each element in the first array.
-   Frequency of value must be same */
+/**
+ * Problem:
+ * You are given two arrays of integers, arr1 and arr2. Write a function that checks whether each value in arr1 has its corresponding value squared in arr2. The frequency of each value in arr1 should match the frequency of its squared value in arr2.
+ * 
+ * Input:
+ * arr1: A list of integers.
+   arr2: A list of integers.
+ * 
+ * Output:
+ * Returns True if every value in arr1 has its corresponding value squared in arr2 with the same       frequency. Otherwise, returns False.
+ * 
+ * Example:
+ * arr1 = [1, 2, 3, 2]
+   arr2 = [1, 4, 9, 4]
+   has_corresponding_squares(arr1, arr2)
+
+   Output: True
+
+ * arr1 = [1, 2, 3]
+   arr2 = [1, 4, 9, 9]
+   has_corresponding_squares(arr1, arr2) 
+
+   Output: False
+ * Constraints:
+ * The arrays may contain negative numbers.
+   The arrays may contain duplicate values.
+   The arrays are not necessarily sorted.
+ */
 
 Method 1:
 

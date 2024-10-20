a7ed49c5cee2486810f2cdc3a20cfffd4e135352
Nipunraj <nipunraj@mozilor.com>
2024-10-20 14:24:30 +0530

    Update question5.js

--- question5.js
+++ question5.js
@@ -39,14 +39,15 @@ Method 1:
 method 2:
 
 function countUniqueValues(arr) {
-  let i = 0;
-  for (let j = 1; j < arr.length; j++) {
-    if (arr[i] !== arr[j]) {
-      i++;
-      arr[i] = arr[j];
-    }
+ if (arr.length === 0) return 0;
+ let i = 0;
+ for (let j=1; j<arr.length; j++){
+  if (arr[i] !== arr[j]){
+   i++;
+   arr[i] = arr[j]
   }
-  return i + 1;
+ }
+ return i + 1;
 }
 
 console.log(countUniqueValues([-1, 2, 2, 2, 3, 4, 8]));
